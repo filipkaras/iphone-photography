aeb29e2a8cb95fc5ce6a877beefd0242523f72de
Filip Karas <filip.karas@gmail.com>
2023-01-11 12:40:51 +0100

    Initial Commit

---
+++ Photography/PhotographyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotographyApp.swift
+//  Photography
+//
+//  Created by Filip Karas on 11/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct PhotographyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
